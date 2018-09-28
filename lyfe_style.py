c145721e328eacf04b6fb67bc31986f7cf58e639
Stanley Chen <dannyshien@gmail.com>
2018-09-27 23:16:59 -0400

    created my first side priject

---
+++ lyfe_style.py
@@ -0,0 +1,13 @@
+# THE ULTIMATE SOLUTION TO SOLVING ONE'S LIFE TROUBLES
+issue = input('Do you have a problem? ')
+
+
+def problem(i) :
+  i = i.lower()
+  if issue == 'yes':
+    input('Can you do something about it? ')
+  else: 
+    issue != 'yes'
+  return 'Then don\'t worry about it'
+
+  hello
